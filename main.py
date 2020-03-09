62c9470f9075d367be9127c713471e43ecbec172
Alex Wohlbruck <alexwohlbruck@gmail.com>
2020-03-08 22:39:14 -0400

    Add UI frame for main options menu

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
+import wx
 import pystray
-from PIL import Image, ImageDraw
 
-def open_options():
-  print('Open options')
+from PIL import Image
+from ui.options import OptionsFrame
 
+app = wx.App()
+
+def open_ui():
+	frm = OptionsFrame(None, title='Hello world')
+	frm.Show()
+	app.MainLoop()
+
+# Create system tray icon
 icon = pystray.Icon('test name')
 icon.title = 'Google Photos Wallpaper'
 icon.icon = Image.open('icon.png')
 icon.menu = pystray.Menu(
-  pystray.MenuItem(
-    text = 'Options',
-    action = open_options,
-    default = True
-  )
+	pystray.MenuItem(
+		text = 'Options',
+		action = open_ui,
+		default = True
+	)
 )
 
 icon.run()
---
+++ ui/options.py
@@ -0,0 +1,54 @@
+import wx
+
+class OptionsFrame(wx.Frame):
+	# Main options GUI
+
+	def __init__(self, *args, **kw):
+		super(OptionsFrame, self).__init__(*args, **kw)
+
+		panel = wx.Panel(self)
+
+		text = wx.StaticText(panel, label='Hello world')
+		font = text.GetFont()
+		font.PointSize += 10
+		font = font.Bold()
+		text.SetFont(font)
+
+		sizer = wx.BoxSizer(wx.VERTICAL)
+		sizer.Add(text, wx.SizerFlags().Border(wx.TOP|wx.LEFT, 25))
+
+		# self.makeMenuBar()
+
+	"""
+	def makeMenuBar(self):
+		fileMenu = wx.Menu()
+		helloItem = fileMenu.Append(-1, "&Hello...\tCtrl-H")
+		fileMenu.AppendSeparator()
+		exitItem = fileMenu.Append(wx.ID_EXIT)
+
+		helpMenu = wx.Menu()
+		aboutItem = helpMenu.Append(wx.ID_ABOUT)
+
+		menuBar = wx.MenuBar()
+		menuBar.Append(fileMenu, "&File")
+		menuBar.Append(helpMenu, "&Help")
+
+		self.SetMenuBar(menuBar)
+
+		self.Bind(wx.EVT_MENU, self.OnHello, helloItem)
+		self.Bind(wx.EVT_MENU, self.OnExit,  exitItem)
+		self.Bind(wx.EVT_MENU, self.OnAbout, aboutItem)
+	"""
+
+	def OnExit(self, event):
+		self.Close(True)
+
+	def OnHello(self, event):
+		wx.MessageBox("Hello again from wxPython")
+
+	def OnAbout(self, event):
+		wx.MessageBox(
+			"This is a wxPython Hello World sample",
+			"About Hello World 2",
+			wx.OK|wx.ICON_INFORMATION
+		)
