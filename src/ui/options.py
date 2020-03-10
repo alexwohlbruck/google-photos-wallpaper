1e7dee14cff223aa2ce16d40c6f51e882804d96d
Alex Wohlbruck <alexwohlbruck@gmail.com>
2020-03-09 20:12:56 -0400

    Replace oauth server with authorization code flow

--- src/ui/options.py
+++ src/ui/options.py
@@ -1,63 +1,12 @@
-import requests
-import json
 import webbrowser
-import threading
-import socket
-
-from flask import Flask, redirect, request, url_for
+import os
 
 import wx
-import google.oauth2.credentials
-from oauthlib.oauth2 import WebApplicationClient
+# import google.oauth2.credentials
+# from oauthlib.oauth2 import WebApplicationClient
+from google_auth_oauthlib.flow import Flow
 
 import src.config as config
-
-
-# Create a web server that opens the Google oAuth login screen
-# and retrieves access and refresh tokens
-		
-app = Flask('Oauth')
-oauthclient = WebApplicationClient(config.GOOGLE_CLIENT_ID)
-google_provider_cfg = requests.get('https://accounts.google.com/.well-known/openid-configuration').json()
-
-@app.route('/oauth')
-def sign_in():
-	request_uri = oauthclient.prepare_request_uri(
-		google_provider_cfg['authorization_endpoint'],
-		redirect_uri = request.base_url + '/callback',
-		scope = ['https://www.googleapis.com/auth/photoslibrary.readonly']
-	)
-
-	return redirect(request_uri)
-
-@app.route('/oauth/callback')
-def callback():
-	code = request.args.get('code')
-
-	print(request.base_url)
-	print(request.url)
-
-	token_url, headers, body = oauthclient.prepare_token_request(
-		google_provider_cfg['token_endpoint'],
-		authorization_response = request.url.replace('http://', ''),
-		redirect_url = request.base_url.replace('http://', ''),
-		code = code
-	)
-
-	token_response = requests.post(
-		token_url,
-		headers = headers,
-		data = body,
-		auth = (config.GOOGLE_CLIENT_ID, config.GOOGLE_CLIENT_SECRET)
-	)
-
-	oauthclient.parse_request_body_response(json.dumps(token_response.json()))
-
-	userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
-	uri, headers, body = oauthclient.add_token(userinfo_endpoint)
-	userinfo_response = requests.get(uri, headers=headers, data=body)
-
-	print(userinfo_response)
 
 class OptionsFrame(wx.Frame):
 	# Main options GUI
@@ -73,48 +22,53 @@ class OptionsFrame(wx.Frame):
 		set_wallpaper_button = wx.Button(panel, label='Set wallpaper', pos=(0, 30))
 		self.Bind(wx.EVT_BUTTON, self.set_wallpaper, set_wallpaper_button)
 
+		
+
 		# self.makeMenuBar()
 
 	def sign_in(self, event):
 
-		# Get unused TCL port
-		s = socket.socket()
-		s.bind(('', 0))
-		port = s.getsockname()[1]
-
-		threading.Thread(target=app.run, kwargs=dict(host='127.0.0.1', port=port, ssl_context='adhoc')).start()
-		webbrowser.open('http://127.0.0.1:' + str(port) + '/oauth')
-
-		""" 
-		credentials = google.oauth2.credentials.Credentials(
-			'access_token'
-			refresh_token='refresh_token',
-			token_uri='token_uri'
-			client_id
-		)
-		"""
-
-		""" client_id = config.GOOGLE_CLIENT_ID
-		client_secret = config.GOOGLE_CLIENT_SECRET
-		scopes = ['https://www.googleapis.com/auth/photoslibrary.readonly']
-
-		credentials = google_auth_oauthlib.get_user_credentials(
-			scopes, client_id, client_secret
+		flow = Flow.from_client_secrets_file(
+			os.path.abspath('src/client_secrets.json'),
+			scopes=[
+				'openid',
+				'https://www.googleapis.com/auth/userinfo.email',
+				'https://www.googleapis.com/auth/photoslibrary.readonly'
+			],
+			redirect_uri='urn:ietf:wg:oauth:2.0:oob'
 		)
 
-		print(credentials) """
+		auth_url, _ = flow.authorization_url(prompt='consent')
+		webbrowser.open(auth_url)
 
-		# 1. Open the link.
-		# 2. Authorize the application to have access to your account.
-		# 3. Copy and paste the authorization code to the prompt.
+		codeInputDialog = wx.TextEntryDialog(None, 'Enter your authorization code', 'Google sign in')
+		
+		if codeInputDialog.ShowModal() == wx.ID_OK:
 
-		# Use the credentials to construct a client for Google APIs.
-		""" from google.cloud import bigquery
+			
+			# The user will get an authorization code. This code is used to get the
+			# access token.
+			code = codeInputDialog.GetValue()
+			flow.fetch_token(code=code)
 
-		bigquery_client = bigquery.Client(
-			credentials=credentials, project="your-project-id"
-		)
-		print(list(bigquery_client.query("SELECT 1").result())) """
+			# You can use flow.credentials, or you can just get a requests session
+			# using flow.authorized_session.
+			session = flow.authorized_session()
+
+
+			favorites = session.post(
+				'https://photoslibrary.googleapis.com/v1/mediaItems:search',
+				json = {
+					'filters': {
+						'featureFilter': {
+							'includedFeatures': ['FAVORITES']
+						}
+					}
+				}
+			).json()
+
+			print(favorites)
+		
 
 	def set_wallpaper(self, event):
 
