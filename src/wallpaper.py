da84fd26866eeb3eda201389f8b9e50c7ee9f6dc
Alex Wohlbruck <alex.wohlbruck@xenial.com>
2022-08-16 23:23:35 -0400

    Fix missing import

--- src/wallpaper.py
+++ src/wallpaper.py
@@ -1,14 +1,14 @@
 from sys import platform
+from os import system
 import ctypes
 
 def set_wallpaper(path):
   if platform == 'darwin':
     # Mac
-    os.system('osascript -e \'tell application "Finder" to set desktop picture to POSIX file "' + path + '"\'')
+    system('osascript -e \'tell application "Finder" to set desktop picture to POSIX file "' + path + '"\'')
   elif platform == 'win32':
     # Windows
     ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 0)
   elif platform == 'linux' or platform == 'linux2':
     # Linux
-    os.system('gsettings set org.gnome.desktop.background picture-uri file://' + path)
-    
+    system('gsettings set org.gnome.desktop.background picture-uri file://' + path)
